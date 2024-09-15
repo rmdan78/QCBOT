000520f2b036e40726f3f2402c13aefca2b0c14a
rmdan78 <mhmhd.ramdan@yahoo.com>
2024-09-15 17:13:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,13 @@
-import cv2
 import streamlit as st
+from PIL import Image
+import numpy as np
 
-st.title("Menampilkan Video dari Kamera di Streamlit")
+st.title("Camera Input Test")
 
-# Tempat untuk video
-video_path = "output.mp4"
+camera = st.camera_input("Capture an image")
 
-# Mengatur OpenCV untuk menangkap video
-cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(video_path, fourcc, 20.0, (640, 480))
+if camera:
+    image = Image.open(camera)
+    img_array = np.array(image)
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        st.write("Tidak dapat menangkap video dari kamera.")
-        break
-
-    # Menulis frame ke file video
-    out.write(frame)
-
-    # Menampilkan video
-    st.video(video_path)
-
-cap.release()
-out.release()
+    st.image(img_array, caption="Captured Image", use_column_width=True)
