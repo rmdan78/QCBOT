cb30c4a614b666b71e27a8bd2db205c66c34c7e0
rmdan78 <mhmhd.ramdan@yahoo.com>
2024-09-15 17:05:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+from PIL import Image
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Kamera Real-Time di Streamlit")
+
+# Input dari kamera
+camera_input = st.camera_input("Ambil Foto")
+
+if camera_input:
+    # Baca gambar dari input kamera
+    img = Image.open(camera_input)
+    st.image(img, caption="Gambar yang Diambil", use_column_width=True)
